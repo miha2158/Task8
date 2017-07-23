06d1eef5601ba9f78620ba17c84cbf491a90b48f
miha2158 <miha.vanya@gmail.com>
2017-07-23 21:08:37 +0500

    сделан поиск в глубину

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -10,6 +10,17 @@ namespace Task8
 {
     class Program
     {
+        static void step(bool[][] graph, bool[] visited, int n)
+        {
+            visited[n] = true;
+
+            foreach (bool[] edge in graph)
+                if(edge[n])
+                    for (int i = 0; i < edge.Length; i++)
+                        if(edge[i] && i != n && !visited[i])
+                            step(graph, visited, i);
+        }
+
         static void Main(string[] args)
         {
         }
